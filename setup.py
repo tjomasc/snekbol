c9256226a3e61fe9337c48ad1c23f92eca52f2bb
Thomas Craig <thomas.craig@liverpool.ac.uk>
2017-03-28 15:50:15 +0100

    Fix issues with setup.py file

--- setup.py
+++ setup.py
@@ -3,13 +3,20 @@ from distutils.core import setup
 
 setup(
     name = 'snekbol',
-    packages = ['sneckbol'],
+    packages = ['snekbol'],
     version = '0.1',
-    description = 'A python based library for reading and writing SBOL 2.1.0 files',
+    description = 'A python based library for reading and writing SBOL 2 files',
     author = 'Thomas Craig',
     author_email = 'thomas.craig@tjc.me.uk',
     url = 'https://github.com/tjomasc/snekbol',
-    download_url = 'github.com/tjomasc/snekbol/archive/0.1.tar.gz',
     keywords = ['SBOL', 'Synthetic Biology'],
-    classifiers = [],
+    classifiers = [
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Operating System :: OS Independent',
+        'Topic :: Scientific/Engineering :: Bio-Informatics',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
 )
